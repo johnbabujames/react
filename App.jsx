7367a20c0e85478e205294c1070faf155388b4bf
John B. James <32309517+johnbabujames@users.noreply.github.com>
2017-10-17 15:30:36 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+    class App extends React.Component {
+      render() {
+        console.log("something");
+        return (
+          <div>John, helloworld!!!</div>
+        );
+      }
+    }
+
+    export default App;
